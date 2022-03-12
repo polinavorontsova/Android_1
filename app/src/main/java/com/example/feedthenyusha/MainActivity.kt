96629f46406344555bd771bf87b8d63d08e73f69
polinavorontsova <polina.vorontsova.21@mail.ru>
2022-03-12 16:32:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/feedthenyusha/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.feedthenyusha
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.feedthenyusha.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
